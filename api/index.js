6c6c4f50a2178611ea0e9992f70a90dc6e2e2b3d
LVSam <sam@learnvest.com>
2013-11-18 23:34:44 -0500

    Adding basic folders, adding user model

---
+++ api/index.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+	app = express.createServer();
+
+app.get('/user/create/:name/:email', function(req, res) {
+	var user = new User({
+		name: req.param.name,
+		email: req.param.email
+	});
+
+	user.setPassword('test');
+
+	user.save(function(err, result) {
+		if (err) throw err;
+	});
+});
+
+app.listen(3000);
