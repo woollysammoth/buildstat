1ebb1bfd01ed5315660e4b305e2a117e1be20330
woollysammoth <amgenstudios@gmail.com>
2013-11-19 00:15:04 -0500

    Update

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+	app = express.createServer();
+
+app.get('/', function(req, res) {
+	res.send("woot!");
+});
+
+app.get('/api/user/create/:name/:email', function(req, res) {
+	var user = new User({
+		name: req.param.name,
+		email: req.param.email
+	});
+
+	user.setPassword('test');
+
+	user.save(function(err, result) {
+		if (err) throw err;
+	});
+});
+
+app.listen(4000);
